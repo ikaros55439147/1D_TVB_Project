72b7ba0e06f95001fc4164d17b3a44a10bfa8513
ikaros55439147 <a119551773@gmail.com>
2025-03-04 03:02:46 +0000

    Initial commit

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+pip install psycopg2
+pip install flask-mail
+pip install PyJWT==2.3.0
+pip install aiosmtpd
+pip install flask
+pip install flask-wtf
+pip install flask-migrate
+pip install flask-sqlalchemy
+pip install flask-babel==2.0.0
+pip install --upgrade pip
+pip install -U djlint
+pip freeze > requirements.txt
+flask db migrate -m "following"
+flask db upgrade
+flask --debug run --host=0.0.0.0
